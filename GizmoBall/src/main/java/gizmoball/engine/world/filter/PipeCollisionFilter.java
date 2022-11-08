b4b4916fae32854574559c7a4cbed7af61852121
D-Sketon <2055272094@qq.com>
2022-11-08 17:35:14 +0800

    fix: 弹性碰撞和管道优化

--- GizmoBall/src/main/java/gizmoball/engine/world/filter/PipeCollisionFilter.java
+++ GizmoBall/src/main/java/gizmoball/engine/world/filter/PipeCollisionFilter.java
@@ -7,12 +7,26 @@ import gizmoball.engine.geometry.shape.AbstractShape;
 import gizmoball.engine.physics.PhysicsBody;
 import gizmoball.engine.world.entity.Ball;
 import gizmoball.engine.world.entity.Pipe;
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
 public class PipeCollisionFilter implements CollisionFilter {
 
     private Vector2 gravity;
+
+    private double ballX;
+    private double ballY;
+
+    private double minX;
+    private double maxX;
+    private double minY;
+    private double maxY;
+    private double radius;
+    private Pipe.PipeDirection pipeDirection;
+
+    private Vector2 linearVelocity;
+
+    public PipeCollisionFilter(Vector2 gravity) {
+        this.gravity = gravity;
+    }
 
     @Override
     public boolean isAllowedBroadPhase(PhysicsBody body1, PhysicsBody body2) {
@@ -35,121 +49,105 @@ public class PipeCollisionFilter implements CollisionFilter {
         Ball ball = (Ball) shape1;
         Pipe pipe = (Pipe) shape2;
 
-        //圆心在管道两侧，直接弹飞
-        if (isOutPipe(ball, pipe)) {
+        double pipeX = pipe.getTransform().getX();
+        double pipeY = pipe.getTransform().getY();
+        ballX = ball.getTransform().getX();
+        ballY = ball.getTransform().getY();
+        radius = ball.getRadius();
+        pipeDirection = pipe.getPipeDirection();
+
+        minX = pipeX - pipe.getHalfWidth();
+        maxX = pipeX + pipe.getHalfWidth();
+        minY = pipeY - pipe.getHalfHeight();
+        maxY = pipeY + pipe.getHalfHeight();
+        linearVelocity = body1.getLinearVelocity();
+        //防止大圆进入小管道
+        if (radius > pipe.getHalfHeight()) {
             return true;
         }
-
+        //圆心在管道两侧，直接弹飞
+        if (isOutPipe()) {
+            return true;
+        }
         //是否与管壁发生碰撞
-        if (!isCollision(ball, pipe)) {
+        if (!isCollision()) {
+            // 没有与管壁发生碰撞并且在管道内，需要反转重力
+            if (isInPipe()) {
+                maintainPipeProperty(body1, body2);
+            }
             return false;
         }
-        //防止大圆进入小管道
-        Vector2[] vertices = pipe.getVertices();
-        for (Vector2 vertex : vertices) {
-            Vector2 transformed = pipe.getTransform().getTransformed(vertex);
-            if (ball.getRadius() - transformed.to(new Vector2(ball.getTransform().x, ball.getTransform().y)).getMagnitude() > Epsilon.E) {
-                penetration.setDepth(penetration.getDepth()/10);
-                return true;
-            }
-        }
-        if (!isInPipe(ball, pipe)) {
+        if (!isInPipe()) {
             if (penetration.getNormal().dot(pipe.getNormals()[0]) < 1e5 * Epsilon.E || penetration.getNormal().dot(pipe.getNormals()[1]) < 1e5 * Epsilon.E) {
                 return false;
             }
             return true;
         }
         //与管壁发生碰撞
+        maintainPipeProperty(body1, body2);
         solveCollision(ball, pipe, penetration);
 
         return true;
     }
 
-    private boolean isInPipe(Ball ball, Pipe pipe) {
-        double pipeX = pipe.getTransform().getX();
-        double pipeY = pipe.getTransform().getY();
-        double ballX = ball.getTransform().getX();
-        double ballY = ball.getTransform().getY();
-
-        double minX = pipeX - pipe.getHalfWidth();
-        double maxX = pipeX + pipe.getHalfWidth();
-        double minY = pipeY - pipe.getHalfHeight();
-        double maxY = pipeY + pipe.getHalfHeight();
+    private boolean isInPipe() {
         return ballX > minX && ballX < maxX && ballY < maxY && ballY > minY;
     }
 
     public void solveCollision(Ball ball, Pipe pipe, Penetration penetration) {
         Vector2 normal = penetration.getNormal();
-        if (pipe.getPipeDirection() == Pipe.PipeDirection.TRANSVERSE) {
+        if (pipeDirection == Pipe.PipeDirection.TRANSVERSE) {
             boolean isHigh = ball.getTransform().y > pipe.getTransform().y;
             if (isHigh) {
-                double maxY = pipe.getTransform().y + pipe.getHalfHeight();
-                penetration.setDepth(ball.getRadius() - (maxY - ball.getTransform().y));
+                penetration.setDepth(radius - (maxY - ball.getTransform().y));
                 normal.x = 0;
                 normal.y = 1;
             } else {
-                double minY = pipe.getTransform().y - pipe.getHalfHeight();
-                penetration.setDepth(ball.getRadius() - (ball.getTransform().y - minY));
+                penetration.setDepth(radius - (ball.getTransform().y - minY));
                 normal.x = 0;
                 normal.y = -1;
             }
         } else {
             boolean isRight = ball.getTransform().x > pipe.getTransform().x;
             if (isRight) {
-                double maxX = pipe.getTransform().x + pipe.getHalfWidth();
-                penetration.setDepth(ball.getRadius() - (maxX - ball.getTransform().x));
+                penetration.setDepth(radius - (maxX - ball.getTransform().x));
                 normal.x = 1;
                 normal.y = 0;
             } else {
-                double minX = pipe.getTransform().x - pipe.getHalfWidth();
-                penetration.setDepth(ball.getRadius() - (ball.getTransform().x - minX));
+                penetration.setDepth(radius - (ball.getTransform().x - minX));
                 normal.x = -1;
                 normal.y = 0;
             }
         }
     }
 
-    public boolean isOutPipe(Ball ball, Pipe pipe) {
-        double pipeX = pipe.getTransform().getX();
-        double pipeY = pipe.getTransform().getY();
-        double ballX = ball.getTransform().getX();
-        double ballY = ball.getTransform().getY();
-
-        if (pipe.getPipeDirection() == Pipe.PipeDirection.TRANSVERSE) {
-            return !((ballY < pipeY + pipe.getHalfHeight()) && (ballY > pipeY - pipe.getHalfHeight()));
+    public boolean isOutPipe() {
+        if (pipeDirection == Pipe.PipeDirection.TRANSVERSE) {
+            return !((ballY < maxY) && (ballY > minY));
         } else {
-            return !((ballX < pipeX + pipe.getHalfWidth()) && (ballX > pipeX - pipe.getHalfWidth()));
+            return !((ballX < maxX) && (ballX > minX));
         }
     }
 
-    public boolean isCollision(Ball ball, Pipe pipe) {
-        double pipeX = pipe.getTransform().getX();
-        double pipeY = pipe.getTransform().getY();
-        double ballX = ball.getTransform().getX();
-        double ballY = ball.getTransform().getY();
-
-        double minX = pipeX - pipe.getHalfWidth();
-        double maxX = pipeX + pipe.getHalfWidth();
-        double minY = pipeY - pipe.getHalfHeight();
-        double maxY = pipeY + pipe.getHalfHeight();
-        double radius = ball.getRadius();
-        Pipe.PipeDirection pipeDirection = pipe.getPipeDirection();
-
-        //横向管道
+    public boolean isCollision() {
         if (pipeDirection == Pipe.PipeDirection.TRANSVERSE) {
-            if (ballY + radius > maxY || ballY - radius < minY) {
-                return true;
-            }
-            return false;
+            return ballY + radius > maxY || ballY - radius < minY;
+        } else {
+            return ballX + radius > maxX || ballX - radius < minX;
         }
+    }
 
-        //竖直管道
-        if (pipeDirection == Pipe.PipeDirection.VERTICAL) {
-            if (ballX + radius > maxX || ballX - radius < minX) {
-                return true;
+    private void maintainPipeProperty(PhysicsBody body1, PhysicsBody body2) {
+        body1.integrateVelocity(gravity.getNegative());
+        if (body1.getShape().getRate() == body2.getShape().getRate()) {
+            if (pipeDirection == Pipe.PipeDirection.TRANSVERSE) {
+                linearVelocity.y = 0;
+            } else if (pipeDirection == Pipe.PipeDirection.VERTICAL) {
+                linearVelocity.x = 0;
             }
-            return false;
         }
-        return true;
+        if (linearVelocity.getMagnitude() < 30) {
+            linearVelocity.multiply(30 / linearVelocity.getMagnitude());
+        }
     }
 }
